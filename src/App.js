c45e5dd2bfaced5dd93da1cef3f55c1a09c17c17
BigBOii <d.barlow@laposte.net>
2022-10-14 15:52:51 +0200

    kill me please

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.scss';
 import Openmap from './components/Openmap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import {
+  BrowserRouter as Router, Routes, Route
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-        <Openmap />
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Dashboard</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/map">Map</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route exact path='*' element={<Login/>}/>
+        <Route exact path='/login' element={<Login/>}/>
+        <Route exact path='/map' element={<Openmap/>}/>
+      </Routes>
+    </Router>
   );
 }
 
