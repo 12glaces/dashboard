3bb4097c8799a9d3100a13016f3e05739961e142
BigBOii <d.barlow@laposte.net>
2022-10-14 16:19:55 +0200

    fix

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className='login' >
+            <form >
+                <h3>Sign In</h3>
+                <div className="mb-3">
+                    <label>Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter email"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter password"
+                    />
+                </div>
+                <div className="d-grid">
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
