a526cdb9e909c502b42326386b8330839eccd0bf
BigBOii <d.barlow@laposte.net>
2022-10-21 18:27:54 +0200

    :sparkles: Added makers

--- src/components/Openmap.js
+++ src/components/Openmap.js
@@ -1,12 +1,43 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { Marker, Popup, MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import React from 'react';
+import { useState } from 'react';
+import L from 'leaflet';
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 const Openmap = () => {
+    const [markers, setMarkers] = useState([
+        {
+          lat: 45.227,
+          lng: 2.213,
+        }
+    ]);
+    const MyComponent = () => {
+        useMapEvents({
+            click: (e) => {
+                setMarkers([...markers, e.latlng]);
+            }
+        })
+        return null
+    }
     return (
         <div>
             <div className='map'>
             <MapContainer center={[45.227, 2.213]} zoom={8} >
+            <MyComponent/>
+            {markers.map((marker, i) => (  // <---- parantheses good
+            <Marker key={`marker-${i}`} position={marker}>
+                <Popup>
+                <span>
+                    Yes this marker is in france. <br /> Oui Oui Oui Baguette hon hon hon
+                </span>
+                </Popup>
+            </Marker>
+            ))}
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
