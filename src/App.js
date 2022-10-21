5adea4295dce1b09f442376c1ccc8396238bb054
BigBOii <d.barlow@laposte.net>
2022-10-21 16:05:47 +0200

    :bug: fixed redirect and sign out button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import {React, useEffect, useState} from 'react';
 import Openmap from './components/Openmap';
 import Login from './components/Login';
 import Container from 'react-bootstrap/Container';
@@ -8,8 +9,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router, Routes, Route
 } from "react-router-dom";
-function App() {
+import { onAuthStateChanged,signOut } from 'firebase/auth';
+import { auth } from './Firebase/firebase';
+const App = () => {
+  
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+    });
+  }, []) 
+  const logout = async () => {
+    await signOut(auth);
+  };
   return (
+    <div className="App">
     <Router>
       <Navbar bg="light" expand="lg">
         <Container>
@@ -17,8 +31,10 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/map">Map</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/map">Map</Nav.Link>              
+              {user? <Nav.Link className="logoff" onClick={logout}> Sign Out </Nav.Link>
+              :               
+              <Nav.Link href="/login">Login</Nav.Link>}
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -26,9 +42,11 @@ function App() {
       <Routes>
         <Route exact path='*' element={<Login/>}/>
         <Route exact path='/login' element={<Login/>}/>
-        <Route exact path='/map' element={<Openmap/>}/>
+        {user? <Route exact path='/map' element={<Openmap/>}/> :
+        null}
       </Routes>
     </Router>
+    </div>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,26 +2,18 @@ import React from 'react';
 import { useState } from "react";
 import {
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut,
+    signInWithEmailAndPassword    
   } from "firebase/auth";
-import { useEffect } from 'react';
 import { auth } from "../Firebase/firebase";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Navigate } from 'react-router-dom';
 const Login = () => {
     const [registerEmail, setRegisterEmail] = useState("");
     const [registerPassword, setRegisterPassword] = useState("");
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
-    const [user, setUser] = useState({});
-
-    useEffect(() => {
-        onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-        });
-    }, []) 
+    const [error, setError] = useState("");
 
     const register = async () => {
         try {
@@ -32,7 +24,7 @@ const Login = () => {
           );
           console.log(user);
         } catch (error) {
-          console.log(error.message);
+            setError(error.message);
         }
       };
     
@@ -44,69 +36,78 @@ const Login = () => {
             loginPassword
           );
           console.log(user);
+          navigateToMap()
         } catch (error) {
-          console.log(error.message);
+            setError(error.message);
         }
-      };
-    
-      const logout = async () => {
-        await signOut(auth);
+      }; 
+
+      const navigateToMap = () => {
+        Navigate('/map');
       };
 
     return (
-        <div className='login' >
-            <form >
-                <h3>Sign In</h3>
-                <div className="mb-3">
-                    <label>Email address</label>
-                    <Form.Control
-                        placeholder="Email..."
-                        onChange={(event) => {
-                            setLoginEmail(event.target.value);
+        <div className='login' >            
+            <div className='login-container' >
+                <form >
+                    <div className="d-grid text-danger">
+                        <p>{error}</p>
+                    </div>
+                    <h3>Sign In</h3>
+                    <div className="mb-3">
+                        <label>Email address</label>
+                        <Form.Control
+                            placeholder="Email..."
+                            onChange={(event) => {
+                                setLoginEmail(event.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label>Password</label>
+                        <Form.Control
+                            placeholder="Password..."
+                            type="password"
+                            onChange={(event) => {
+                            setLoginPassword(event.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="d-grid">
+                        <Button onClick={login}> Login</Button>
+                    </div>
+                </form>
+                <p>Or</p>
+                <form >
+                    <h3>Sign up</h3>
+                    <div className="mb-3">
+                        <label>Email address</label>
+                        <Form.Control
+                            placeholder="Email..."
+                            onChange={(event) => {
+                                setRegisterEmail(event.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label>Password</label>
+                        <Form.Control
+                            placeholder="Password..."
+                            type = "password"
+                            onChange={(event) => {
+                                setRegisterPassword(event.target.value);
                         }}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Password</label>
-                    <Form.Control
-                        placeholder="Password..."
-                        type="password"
-                        onChange={(event) => {
-                          setLoginPassword(event.target.value);
-                        }}
-                    />
-                </div>
-                <div className="d-grid">
-                    <Button onClick={login}> Login</Button>
-                </div>
-            </form>
-            <p>Or</p>
-            <form >
-                <h3>Sign up</h3>
-                <div className="mb-3">
-                    <label>Email address</label>
-                    <Form.Control
-                        placeholder="Email..."
-                        onChange={(event) => {
-                            setRegisterEmail(event.target.value);
-                        }}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label>Password</label>
-                    <Form.Control
-                        placeholder="Password..."
-                        type = "password"
-                        onChange={(event) => {
-                            setRegisterPassword(event.target.value);
-                    }}
-                    />
-                </div>
-                <div className="d-grid">
-                    <Button onClick={register}> Create User</Button>
-                </div>
-            </form>
-            {user? <Button className="logoff" onClick={logout}> Sign Out </Button>: null}
+                        />
+                    </div>
+                    <div className="d-grid">
+                        <Button onClick={register}> Create User</Button>
+                    </div>                    
+                </form>
+                </div>     
+                <p>You can create an account or use this test account:</p>
+                <p>email: test@test.com</p>
+                <p>password: testtest</p>  
+                
         </div>
     );
 };
